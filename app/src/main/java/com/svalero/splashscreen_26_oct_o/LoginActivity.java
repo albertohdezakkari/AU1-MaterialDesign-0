4f7c4877e9e51b16f6814a08925defe1e6582267
Alberto Herandez <albertohdez@gmail.com>
2020-10-26 18:13:29 +0100

    Initial commit

---
+++ app/src/main/java/com/svalero/splashscreen_26_oct_o/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.svalero.splashscreen_26_oct_o;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final int SCREEN = 2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
---
+++ app/src/main/java/com/svalero/splashscreen_26_oct_o/MainActivity.java
@@ -0,0 +1,32 @@
+package com.svalero.splashscreen_26_oct_o;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int SCREEN = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Lanzar un proceso en 2º plano que se active a los 4sg
+        // package--> Manejar hilos
+        final Handler handler = new Handler();
+        handler.postDelayed(
+                new Runnable() { // Interface
+                    @Override
+                    public void run() {
+                        // Cargar la 2ª pantalla
+                        Intent navegar = new Intent(
+                                getBaseContext(), LoginActivity.class);
+                        startActivity(navegar);
+                    }
+               }
+                , 4000
+        );
+    }
+}
