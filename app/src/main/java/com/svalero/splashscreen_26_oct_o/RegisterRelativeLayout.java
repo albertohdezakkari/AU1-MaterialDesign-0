7e6a5e9de2b0dd71f951da3f93bcf74f1d894963
Alberto Herandez <albertohdez@gmail.com>
2020-11-03 21:49:20 +0100

    1º commit

--- app/src/main/java/com/svalero/splashscreen_26_oct_o/RegisterRelativeLayout.java
+++ app/src/main/java/com/svalero/splashscreen_26_oct_o/RegisterRelativeLayout.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class LoginActivity extends AppCompatActivity {
-    private static final int SCREEN = 2;
+public class RegisterRelativeLayout extends AppCompatActivity {
+    private static final int SCREEN = 3;
     private Button btnEnviar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_register_relative_layout);
         //3º--> Inicializar
         initComponents();
 
@@ -22,14 +22,16 @@ public class LoginActivity extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Cargar la 2ª pantalla
+                // Cargar la 3ª pantalla
                 Intent navegar = new Intent(
-                        getBaseContext(), RegisterRelativeLayout.class);
+                        getBaseContext(), LoginContraintLayout.class);
                 startActivity(navegar);
             }
         });
     }
     private void initComponents(){
-        btnEnviar = findViewById(R.id.btnEnviar);
+        btnEnviar = findViewById(R.id.btnEnviarConstraint);
+        LinearLayout capa2 = findViewById(R.id.capa2);
+        //capa2.setEnabled(true);
     }
 }
